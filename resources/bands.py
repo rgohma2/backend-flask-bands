8e1776b60bae9de2048f6dab41db8240d6ec5d4d
Randy Gohmann <rgohma2@gmail.com>
2020-02-11 17:08:22 -0600

    can create new band

--- resources/bands.py
+++ resources/bands.py
@@ -1,9 +1,15 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request
 
 
 bands = Blueprint('bands', 'bands')
 
-@bands.route('/')
+@bands.route('/', methods=['GET'])
 def bands_index():
 	return 'bands resource working!'
+
+@bands.route('/', methods=['POST'])
+def create_band():
+	payload = request.get_json()
+	band = models.Bands.create(year_formed=payload['year_formed'], vocals=payload['vocals'], guitar=payload['guitar'], drums=payload['drums'])
+	print(band)
